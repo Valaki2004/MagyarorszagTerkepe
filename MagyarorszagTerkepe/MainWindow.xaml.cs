843a153a988228b08ce2ec056802afa4f11faea6
vtg <varastothgergo@ktch.hu>
2024-05-13 10:09:56 +0200

    Add project files.

---
+++ MagyarorszagTerkepe/MainWindow.xaml.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO;
+namespace MagyarorszagTerkepe
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        double[] x_ek = new double[3137];
+        //List<double> x_ek2 = new List<double>(); //amennyiben nem tudnánk előre, hogy hány település is van ez egy szerencsésebb tároló
+        double[] y_ok = new double[3137];
+        //List <double> y_ok2 = new List<double>();
+        public MainWindow()
+        {
+            InitializeComponent();
+            StreamReader file = new StreamReader("helysegek_koordinatai.csv");
+            int db = 0;
+            file.ReadLine(); //hogy a címsort figyelmenkívül hagyjuk
+            while(!file.EndOfStream)
+            {
+                string sor=file.ReadLine();
+                string[] reszek = sor.Split(';'); //név;x;y
+                string[] fpszp = reszek[1].Split(new char[] { ':', '.' });
+                double x = int.Parse(fpszp[0]); //ez a fok egész értéke
+                x += (double.Parse(fpszp[1]) / 60);
+                x += (double.Parse(fpszp[2]) / 6000);
+                x_ek[db] = x;
+                //x_ek2.Add(x);
+                fpszp = reszek[2].Split(new char[] { ':', '.' });
+                double y = int.Parse(fpszp[0]); //ez a fok egész értéke
+                y += (double.Parse(fpszp[1]) / 60);
+                y += (double.Parse(fpszp[2]) / 6000);
+                y_ok[db] = y;
+                //y_ok2.Add(y);
+                db++;
+            }
+            file.Close();
+            //MessageBox.Show($"Beolvasott sorok száma: {db}");
+        }
+        private void Terkep(object sender, MouseButtonEventArgs e)
+        {
+            //MessageBox.Show("Kattintás");
+            double xmeret = Rajzlap.Width / 7;
+            double ymeret = Rajzlap.Height / 3;
+            for (int i = 0; i < x_ek.Length; i++)
+            {
+                Line vonal = new Line();
+                vonal.X1 = (x_ek[i]-16)*xmeret;
+                vonal.Y1 = (48.7-y_ok[i])*ymeret;
+                vonal.X2 = vonal.X1 + 1;
+                vonal.Y2 = vonal.Y1 + 1;
+                vonal.Stroke = Brushes.Black;
+                vonal.StrokeThickness = 1;
+                Rajzlap.Children.Add(vonal);
+            }
+        }
+    }
+}
